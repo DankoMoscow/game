42e5d8e49f3f76b7b8105f0a9ac473a5a5dfcd44
Danila <danko_blue@mail.ru>
2022-11-12 17:16:20 +0300

    first version, i changed something andd added new methods

--- game_rogue.py
+++ game_rogue.py
@@ -29,14 +29,40 @@ class Character:
         self.coords = None #хрен знает, может убрать?
         self.speed = speed
 
-    """def rases(self):
-        if type_of_person == "человек" or "Человек":
-            self.health = 100
-            self.armor = 10
-            self.speed = 1
-            self.attack_damage = 20
-            self.attack_range = 1"""
 
+"""тут я хочу прописать метод атаки персонажа"""
+    #def start_attack (self):
+
+
+def start_move(self, direction): #direction - это направление движения, которое можно реализовать в main цикле
+    if direction == 'w':
+        if self.second_coor > 1: #тут условия, чтобы не выходил за границы квадрата, но надо ещё проработать, чтобы он не мог на стены лезть и так далее
+            self.second_coor -= 1
+        else:
+            self.second_coor = self.second_coor
+
+    elif direction == 's':
+        if self.second_coor < height:
+            self.second_coor += 1
+        else:
+            self.second_coor = self.second_coor
+
+    elif direction == 'd':
+        if self.first_coor < width:
+            self.first_coor += 1
+        else:
+            self.first_coor = self.first_coor
+
+    elif direction == 'a':
+        if self.first_coor > 1:
+            self.first_coor -= 1
+        else:
+            self.first_coor = self.first_coor
+
+class Monster(Character):
+    def __init__(self, name, health, armor, attack_range, attack_damage, speed, radius_finding):
+        self.health = health
+        self.radius_finding  = radius_finding
 
 def main():
 
@@ -54,9 +80,10 @@ def main():
     elif type_of_person == 'гном' or 'Гном':
         person1 = Character(name_person, 120, 12, 1, 22, 3)
 
+
     print('''Имя: {},уровень жизни : {}, уровень защиты: {}, дальность атаки: {}, урон: {} и скорость: {}
             '''.format(person1.name, person1.health, person1.armor, person1.attack_range, person1.attack_damage, person1.speed))
-
+"""тут надо еще создать экземпляры класса монстров"""
 main()
 
 
