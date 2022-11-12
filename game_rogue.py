4f1df1e60cabebd6f000a1dcf131d2bf11c419be
Danila <danko_blue@mail.ru>
2022-11-12 22:54:09 +0300

    first version, i changed something andd added new methods and class. Create fight with monster

--- game_rogue.py
+++ game_rogue.py
@@ -25,11 +25,17 @@ class room:
         self.cord0 = [self.X0,self.Y0]
         self.cord = [self.X,self.Y]
 
-    def update(self,new_cord):
+    def update(self,new_cord, first_coor_monster, second_coor_monster):
         self.matrix[self.Y0][self.X0] = ' '
         self.Y0 =  new_cord[1]
         self.X0 = new_cord[0]
+        self.f_coor_monster = first_coor_monster
+        self.s_coor_monster = second_coor_monster
         self.matrix[self.Y0][self.X0] = '1'
+        self.matrix[self.f_coor_monster][self.s_coor_monster] = '*' #хочу, чтобы звёздочка убиралась на каждой итерации
+        """
+        тут пытаюсь создать обновляющееся поле для монстра
+        """
 
     def display(self):
         for i in range(0, len(self.matrix)):
@@ -95,6 +101,9 @@ class Character:
             self.attack_damage = 22
         return self.attack_damage
 
+    def start_attack(self, another):
+        another.health -= self.attack_damage
+
 #тут будут методы персонажа
 
     def move(direction, cord0, cord, exit, input):  # direction - это направление движения, которое можно реализовать в main цикле
@@ -123,10 +132,17 @@ class Character:
 
 
 class Monster(Character):
-    def __init__(self, first_coor_monster, second_coor_monster):
+    def __init__(self, name_monster,first_coor_monster, second_coor_monster):
         self.first_coor_monster = first_coor_monster
         self.second_coor_monster = second_coor_monster
+        self.name_monster = name_monster
+        self.health = 0
+        self.armor = 0
 
+    def get_monster_health(self):
+        if self.name_monster == 'Гоблин':
+            self.health = 40
+        return self.health
 
     def move(self):
         global key_list, direction
@@ -155,6 +171,9 @@ class Monster(Character):
             else:
                 self.first_coor_monster = self.first_coor_monster
 
+        return self.first_coor_monster, self.second_coor_monster
+
+
 class Items:
     def __init__(self, room, value, rare):
         self.room = room
@@ -174,7 +193,7 @@ def main():
     type_of_person = str(input(f'Выберите расу персонажа: человек, эльф или гном '))
     name_person= input('Назовите Вашего персонажа: ')
 
-    person1 = Character(name_person, type_of_person)
+    person1 = Character(name_person, type_of_person) #создаём экземпляр класса
     person1.get_armor()
     person1.get_attack_damage()
     person1.get_damage_range()
@@ -184,9 +203,16 @@ def main():
     print(f'''Имя: {person1.name}, уровень жизни : {person1.health}, уровень защиты: {person1.armor}, 
 дальность атаки: {person1.attack_range}, урон: {person1.attack_damage} и скорость: {person1.speed}''')
 
-    first_coor_monster = random.randint(1, 5)
-    second_coor_monster = random.randint(1, 5)
-    monster = Monster(first_coor_monster, second_coor_monster)
+    first_coor_monster_random = random.randint(1, 5) #создаём рандомного монстра
+    second_coor_monster_random = random.randint(1, 5)
+    monster = Monster("Гоблин", first_coor_monster_random, second_coor_monster_random) #указываем ему его координаты
+
+    """
+    это боёвка
+    """
+    monster.get_monster_health()
+    person1.start_attack( monster)
+    print(monster.health)
 
     rooms = [0]*3
     for i in range(3):
@@ -196,8 +222,8 @@ def main():
         n = i
         while True:
             new_cord = Character.move(input('Введите напраление "w", "a", "s", "d"'), rooms[n].cord0, rooms[n].cord, rooms[n].exit, rooms[n].input)
-            monster.move()
-            rooms[n].update(new_cord)
+            new_cord1, new_cord2 = monster.move()
+            rooms[n].update(new_cord, new_cord1, new_cord2)
             if rooms[n].input[0]==rooms[n].cord0[0] and rooms[n].input[1]==rooms[n].cord0[1] and n!=0:
                 n=n-1
 
