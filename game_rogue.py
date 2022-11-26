9128091f227e176e04a06a8b9049974e576c35eb
Danila <danko_blue@mail.ru>
2022-11-26 20:21:08 +0300

    remove the -main- function and added it to the file with pygame

--- game_rogue.py
+++ game_rogue.py
@@ -7,8 +7,8 @@ key_list = ['w', 'a', 's', 'd']
 
 class room:
     def __init__(self):
-        self.X = random.randint(6, 15)
-        self.Y = random.randint(6, 15)
+        self.X = random.randint(8, 16)
+        self.Y = random.randint(6, 12)
         self.f_item_coord = 0
         self.s_item_coord = 0
         self.matrix = [[' '] * self.X for i in range(self.Y)]
@@ -294,7 +294,7 @@ class Items:
             self.range_attack = 1
 
     def loot(self,new_cord_monster,new_cord,alive_or_ded):
-        if (fabs(new_cord_monster[0]-new_cord[0]) == 1 or fabs(new_cord_monster[1]-new_cord[1]) == 1) and alive_or_ded=='мёртв':
+        if (fabs(new_cord_monster[0]-new_cord[0]) <= 1 and fabs(new_cord_monster[1]-new_cord[1]) <= 1) and alive_or_ded=='мёртв':
             self.action_predmet = input(f'Хотите взять {self.name_items} ? Да/нет')
             if self.action_predmet == ('да' or 'Да'):
                 self.state = 'взят'
@@ -342,105 +342,3 @@ def making_monster():
     monsterX.get_monster_speed()
     monsterX.alive_or_ded()
     return monsterX
-
-
-def main():
-    global N, rooms, monster, item, person1
-    privetstvie()
-    person1 = making_character()
-    """
-    может быть принты перенести в сам pygame
-    """
-    print(f'''Имя: {person1.name}, уровень жизни : {person1.health}, уровень защиты: {person1.armor}, 
-дальность атаки: {person1.attack_range}, урон: {person1.attack_damage} и скорость: {person1.speed}''')
-
-    N = 3 #количество комнат
-    rooms = [0] * N
-    monster = [0] * N
-    item = [0] * N
-
-    for i in range(N):
-        rooms[i] = room()
-        rooms[i].generation()
-        rooms[i].display()
-        """
-        создаём экземпляр класса предметов
-        """
-        list_items = ['Кинжал', 'Короткий лук', 'Великий меч','Длинный лук']
-        name_items = random.choice(list_items)
-        item[i] = Items(name_items)
-        item[i].parametres()
-
-        n = i
-
-        monster[i] = making_monster()
-
-        while True:
-            print(f'В начале хода у Вас: {person1.health} здоровья')
-            attack_action = False
-
-            if (fabs(rooms[n].X0_monster0 - rooms[n].X0) <= monster[n].attack_range) and (
-                                fabs(rooms[n].Y0_monster0 - rooms[n].Y0) <= monster[n].attack_range) and monster[n].health > 0:
-                monster[n].attack_result(person1)
-                attack_action = True
-                print(f'Здоровье персонажа после атаки: {person1.health}')
-                if person1.health <= 0:
-                    print('Вы погибли, игра закончена')
-                    raise SystemExit
-
-            if (fabs(rooms[n].X0_monster0 - rooms[n].X0) <= person1.attack_range) and (
-                    fabs(rooms[n].Y0_monster0 - rooms[n].Y0) <= person1.attack_range) and monster[n].health > 0:
-                action = input('Желаете напасть на врага? Если да - нажмите f, если хотите защититься - нажмите g')
-                if  action == ('f' or 'F'):
-                    person1.attack_result(monster[n])
-                    print(f'Здоровье монстра после атаки: {monster[n].health}')
-
-                elif action == ('g' or 'G') and attack_action == True:
-                    person1.start_defence(monster[n])
-
-                elif  action == ('g' or 'G') and attack_action == False:
-                    print('Враг не наносил Вам урон')
-                    if person1.health <= 0:
-                        print('Вы погибли, игра закончена')
-
-            if monster[n].health > 0:
-                print(f'Вы видите в комнате {monster[n].name}')
-
-            for j in range(person1.speed):
-                new_cord = person1.move(input('Введите напраление "w", "a", "s", "d"'), rooms[n].cord0, rooms[n].cord,
-                                        rooms[n].exit, rooms[n].input, rooms[n].cord_monster0)
-
-                rooms[n].update_character(new_cord)
-                if rooms[n].input[0] == rooms[n].cord0[0] and rooms[n].input[1] == rooms[n].cord0[1] and n != 0:
-                    n = n - 1
-
-                elif rooms[n].exit[0] - 1 == rooms[n].cord0[0] and rooms[n].exit[1] == rooms[n].cord0[1]:
-                    n = n + 1
-
-                if rooms[i].exit[0] - 1 == rooms[i].cord0[0] and rooms[i].exit[1] == rooms[i].cord0[1]:
-                    break
-                rooms[n].display()
-
-            if rooms[i].exit[0] - 1 == rooms[i].cord0[0] and rooms[i].exit[1] == rooms[i].cord0[1]:
-                break
-
-            if monster[n].health > 0:
-                for m in range(monster[i].speed):
-                    new_cord_monster = monster[n].move(rooms[n].cord_monster0, rooms[n].cord, new_cord)
-                    rooms[n].update_monster(new_cord_monster, monster[i].alive_or_ded())
-                    rooms[n].display()
-            else:
-                rooms[n].update_monster(new_cord_monster, monster[n].alive_or_ded())
-                rooms[n].display()
-
-            print(monster[n].name, monster[n].alive_or_ded())
-
-            if item[n].state =='лежит':
-                item[n].loot(new_cord_monster, new_cord, monster[n].alive_or_ded())
-                characteristics = [item[n].range_attack,item[n].damage]
-                person1.attack_range = characteristics[0]
-                person1.attack_damage = characteristics[1]
-
-    print('ВЫ ПОБЕДИЛИ !!!')
-
-main()
